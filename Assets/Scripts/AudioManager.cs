bc94450469d8fac14e48169f61ddbdbc0af628ce
Iker8rc <ikerrodriguez.fg@gmail.com>
2026-03-11 20:22:13 +0100

    paneles de lore y script de sonido

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    private AudioSource musicSource;
+    private AudioSource ambientSource;
+    [SerializeField]
+    private GameObject SFXPrefab;
+    private float musicVolume;
+    private float sfxVolume; 
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        musicSource = gameObject.AddComponent<AudioSource>();
+        ambientSource = gameObject.AddComponent<AudioSource>();
+
+        musicVolume = 0.6f;
+        sfxVolume = 1f;
+    }
+    public void PlayMusic(AudioClip _music)
+    {
+        musicSource.clip = _music;
+        musicSource.volume = musicVolume;
+        musicSource.Play();
+    }
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+    public void PlayAmbientSound(AudioClip _ambient)
+    {
+        ambientSource.clip = _ambient;
+        ambientSource.volume = sfxVolume;
+        ambientSource.Play();
+    }
+    public void StopAmbientSound()
+    {
+        ambientSource.Stop();
+    }
+
+    public void PlaySFX(AudioClip _sfx, Vector3 _position)
+    {
+        GameObject SFXClone = Instantiate(SFXPrefab, _position, Quaternion.identity);
+        SFXClone.GetComponent<AudioSource>().clip = _sfx;
+        SFXClone.GetComponent <AudioSource>().volume = sfxVolume;
+        SFXClone.GetComponent <AudioSource>().Play(); 
+        Destroy(SFXClone, _sfx.length);
+    }
+    public void SetMusicVolume(float _volume)
+    {
+        musicVolume = _volume;
+        musicSource.volume = _volume;
+    }
+
+    public void SetSFXVolume(float _volume)
+    {
+        sfxVolume = _volume;
+        ambientSource.volume = _volume;
+    }
+}
