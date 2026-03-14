e306d57918c5565b9ef065477b1f4d17ad44de4d
AlbertVichFG <albertvich.fg@gmail.com>
2026-03-14 04:46:58 +0100

    Pis1

--- Assets/Scripts/Inventori/PickupItem.cs
+++ Assets/Scripts/Inventori/PickupItem.cs
@@ -1,13 +1,33 @@
 using UnityEngine;
+using System.Collections;
 
 public class PickupItem : Interactable
 {
 
     [SerializeField] private ItemData item;
-
     public override void Interact(PlayerController player)
     {
+        player.StartCoroutine(PickupSequence(player));
+    }
+
+    IEnumerator PickupSequence(PlayerController player)
+    {
+        player.StopMovement();
+
+        Animator anim = player.GetComponentInChildren<Animator>();
+
+        anim.SetTrigger("PickItem");
+
+        yield return null;
+
+        float length = anim.GetCurrentAnimatorStateInfo(0).length;
+
+        yield return new WaitForSeconds(length);
+
         InventoryManager.instance.AddItem(item);
+
+        player.UnlockMovement();
+
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Puzzles/Pis1/Buzz.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class BathtubPuzzle : Interactable
+{
+    [Header("Item correcte")]
+    [SerializeField] private ItemData requiredItem;
+
+    [Header("Animació martell")]
+    [SerializeField] private Animator hammerAnimator;
+
+    [Header("Collider banyera")]
+    [SerializeField] private Collider bathtubCollider;
+
+    [Header("Dialogue")]
+    [SerializeField] private DialogueUI dialogueUI;
+
+    [TextArea]
+    [SerializeField] private string noItemLine;
+
+    [TextArea]
+    [SerializeField] private string wrongItemLine;
+
+    private bool solved = false;
+
+    public override void Interact(PlayerController player)
+    {
+        Debug.Log("BATHTUB INTERACT");
+
+        dialogueUI.SetPlayer(player);
+
+        if (solved)
+        {
+            Debug.Log("Puzzle ja resolt");
+            return;
+        }
+
+        if (!InventoryManager.instance.HasItemInHand())
+        {
+            Debug.Log("NO ITEM");
+            dialogueUI.ShowLine(noItemLine);
+            return;
+        }
+
+        ItemData item = InventoryManager.instance.GetItemInHand();
+
+        Debug.Log("Item a la mà: " + item.name);
+        Debug.Log("Item requerit: " + requiredItem.name);
+
+
+
+        Debug.Log("ITEM CORRECTE resolent puzzle");
+
+        StartCoroutine(SolvePuzzle());
+    }
+
+    IEnumerator SolvePuzzle()
+    {
+        solved = true;
+
+        Debug.Log("Eliminant item inventari");
+
+        InventoryManager.instance.RemoveItemInHand();
+
+        Debug.Log("Animació martell");
+
+        if (hammerAnimator != null)
+        {
+            hammerAnimator.SetBool("Flota", true);
+        }
+        else
+        {
+            Debug.LogError("hammerAnimator NULL");
+        }
+
+        yield return new WaitForSeconds(1.2f);
+
+        Debug.Log("Desactivant collider banyera");
+
+        if (bathtubCollider != null)
+        {
+            bathtubCollider.enabled = false;
+        }
+        else
+        {
+            Debug.LogError("bathtubCollider NULL");
+        }
+    }
+}
+
