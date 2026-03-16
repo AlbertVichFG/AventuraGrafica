6d6a16a56064a4c2fd0932cf20056dbf14f495bc
rogergilfg <rogergil.fg@gmail.com>
2026-03-16 01:44:28 +0100

    basement cams check

--- Assets/Scripts/LVL0/CameraTrigger.cs
+++ Assets/Scripts/LVL0/CameraTrigger.cs
@@ -3,12 +3,32 @@ using UnityEngine;
 public class CameraTrigger : MonoBehaviour
 {
     public Camera targetCamera;
+    private bool isActive = false;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            isActive = true;
             CameraManager.Instance.SwitchTo(targetCamera);
         }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (isActive && other.CompareTag("Player"))
+        {
+            // Si la cámara activa ya no es la nuestra, reclamarla
+            if (!targetCamera.gameObject.activeSelf)
+            {
+                CameraManager.Instance.SwitchTo(targetCamera);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            isActive = false;
     }
 }
